62531ccb30a5c0c1a2e6c3827b7bc03db72d934a
jackcav1 <59706122+jackcav1@users.noreply.github.com>
2022-02-14 21:24:17 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 DISTR=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
-if [[ $DISTR -e "CentOS Linux" ]]; then
+if [[ $DISTR == "CentOS Linux" ]]; then
   echo "No action taken..."
   echo "Are you sure this is a CentOS system?"
   exit 1
